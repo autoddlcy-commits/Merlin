f0765abfec03d5bfade7fed0d9b039ab0785b2c9
YING CAO <autoddlcy@gmail.com>
2026-03-20 18:29:23 +0800

    Update download_data.py

--- merlin/data/download_data.py
+++ merlin/data/download_data.py
@@ -4,8 +4,12 @@ from merlin.utils import download_file
 def download_sample_data(data_dir):
     print("Downloading sample data to {}".format(data_dir))
     file_path = download_file(
-        repo_id="stanfordmimi/Merlin",
-        filename="image1.nii.gz",
+        #从哪下载
+        repo_id="stanfordmimi/Merlin",    #Hugging Face关于repo_id的命名规则：组织名(或用户名)/仓库名；这里是斯坦福 MIMI 实验室的 Merlin 仓库 
+        #下载啥
+        filename="image1.nii.gz",         #image1.nii.gz：举例，根据下载文件不同修改
+        #下载到哪
         local_dir=data_dir,
     )
+    #file_path：load_dir+filename
     return file_path
